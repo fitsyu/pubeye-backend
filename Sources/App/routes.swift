5dbc6aca9e6aa9d51c0451955144ed0b749a338d
Fitsyu Two <fitsyu2@gmail.com>
2019-10-31 23:40:41 +0700

    config Leaf

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,8 +8,12 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+//    router.get("hello") { req in
+//        return "Hello, world!"
+//    }
+    
+    router.get("hello") { req -> Future<View> in
+        return try req.view().render("hello", ["name": "Leaf Ninja"])
     }
 
     // Example of configuring a controller
